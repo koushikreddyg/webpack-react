b123883cee99ef48c554c08a60af2a2028f48a8c
koushikui <koushikreddygunapati@gmail.com>
2019-02-07 22:33:42 -0600

    preinstall.js

--- internals/preinstall.js
+++ internals/preinstall.js
@@ -3,6 +3,6 @@ const { exec } = require("child_process");
 exec("npm -v", (err, std) => {
   if (err) throw err;
   if (parseFloat(std) < 6) {
-    // throw new Error("check the npm version");
+    throw new Error("check the npm version");
   }
 });
