b046454919850ee66c9f25a6b5ffaf2a3515b099
koushikui <koushikreddygunapati@gmail.com>
2019-02-07 22:13:41 -0600

    added jest husky

--- internals/preinstall.js
+++ internals/preinstall.js
@@ -2,7 +2,7 @@ const { exec } = require("child_process");
 
 exec("npm -v", (err, std) => {
   if (err) throw err;
-  if (parseFloat(std) > 6) {
-    throw new Error("check the npm version");
+  if (parseFloat(std) < 6) {
+    // throw new Error("check the npm version");
   }
 });
