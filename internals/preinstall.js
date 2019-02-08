9a8488255e099f8aa028a537b0a40a7659f4471e
koushikui <koushikreddygunapati@gmail.com>
2019-02-07 23:36:53 -0600

    internals/yarnCheck

--- internals/preinstall.js
+++ internals/preinstall.js
@@ -3,6 +3,12 @@ const { exec } = require("child_process");
 exec("npm -v", (err, std) => {
   if (err) throw err;
   if (parseFloat(std) < 6) {
-    throw new Error("check the npm version");
+    console.error("node version is below 6");
+    process.exit(1);
   }
 });
+
+if (process.env.npm_execpath.indexOf("yarn") > -1) {
+  console.error("You must not use Yarn to install dependencies:");
+  process.exit(1);
+}
