cad9d6f54f49247fdcb73560541cabaf2f032982
koushikui <koushikreddygunapati@gmail.com>
2019-02-18 10:32:35 -0600

    added axios mock

---
+++ src/tests/networking/index.test.js
@@ -0,0 +1,10 @@
+import { sendRequest } from "../../networking";
+
+test("should fetch users", () => {
+  sendRequest({
+    url: "",
+    responseType: "",
+    mapper: "",
+    method: ""
+  }).then(resp => console.log(resp));
+});
